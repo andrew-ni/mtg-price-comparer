856f22f42a6c182336105a9deefa8f452e7b401f
Andrew Ni <niandrewdev@gmail.com>
2020-08-30 13:58:39 -0700

    Add url building

--- mtg-price-comparer.py
+++ mtg-price-comparer.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 import bs4
@@ -8,7 +9,7 @@ def main():
     filepath = sys.argv[1]
     cards = get_cards_from_file(filepath)
     print(cards)
-    card_kingdom_url = 'https://www.cardkingdom.com/builder/mtg?maindeck=4+Ancient+Den%0D%0A2+Arcbound+Ravager%0D%0A4+Welding+Jar%0D%0A4+Ornithopter&format=all'
+    card_kingdom_url = get_card_kingdom_url(cards)
     print(card_kingdom_url)
     response = requests.get(card_kingdom_url)
     print(response.status_code)
@@ -17,10 +18,14 @@ def main():
 
 def get_cards_from_file(filepath):
     cards = []
+    pattern = re.compile(r"^(\d+) (.*)$")
     with open(filepath) as f:
         lines = f.readlines()
 
     for line in lines:
+        if not pattern.match(line):
+            print(f"Not valid: {line}")
+            continue
         tokens = line.strip().split(' ', 1)
         cards.append((tokens[0], tokens[1]))
 
